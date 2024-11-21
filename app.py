6b7d283119edd382789e5e86b80421322d3596d7
jwells167 <jwells167@bloomberg.net>
2024-11-21 15:59:23 +0000

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 index_html_path = 'index.html'
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template(index_html_path)
 
