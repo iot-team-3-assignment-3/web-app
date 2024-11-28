1278b72d000b9f5ee428789a5e7f41a35e653408
jwells167 <133053713+JamieWells1@users.noreply.github.com>
2024-11-28 12:33:54 +0000

    added react template

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, jsonify
 import json
+import requests
 
 app = Flask(__name__)
-index_path = "index.tsx"
+index_path = "index.html"
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template(index_path)
+    request = requests.get('https://api.carbonintensity.org.uk/regional')
+    return render_template(index_path, request=request)
 
 
 def main():
